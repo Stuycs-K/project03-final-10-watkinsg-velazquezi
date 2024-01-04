feb53fc0abd243e6b6a3a2bc3c7136145d439436
Israel Velazquez <Ivel2018@gmail.com>
2024-01-04 15:46:48 -0500

    Pushing random and bogosort start

--- networking.c
+++ networking.c
@@ -1,5 +1,30 @@
 #include "networking.h"
 
+void bogoSort(int arr[], int size) {
+  while (1) {
+    for (int i=0; i<size; i++) {
+      
+    }
+  }
+}
+
+int ran(int size) {
+  int r_file = open("/dev/random", O_RDONLY , 0);
+  int buff[size];
+  size = read(r_file, buff, size);
+  if(size == -1)err();
+  int number = 0;
+  int increase = 0;
+  for (int i=1; i<=size; i++) {
+    number+=buff[i-1]<<increase;
+    if (increase==0) {
+      increase++;
+    }
+    increase<=2;
+  }
+  return number;
+}
+
 void err(int i, char*message){
   if(i < 0){
 	  printf("Error: %s - %s\n",message, strerror(errno));
