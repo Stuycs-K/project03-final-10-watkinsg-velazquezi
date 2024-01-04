03e37e323abc977409ce3ceed544b021388af522
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-01-04 15:35:43 -0500

    begin server (copied from lab

--- networking.c
+++ networking.c
@@ -6,3 +6,33 @@ void err(int i, char*message){
   	exit(1);
   }
 }
+
+int server_setup() {
+  //setup structs for getaddrinfo
+  struct addrinfo * hints, * results;
+  hints = calloc(1, sizeof(struct addrinfo)); // error in vscode for this..??
+  hints->ai_family = AF_INET;
+  hints->ai_socktype = SOCK_STREAM;
+  hints->ai_flags = AI_PASSIVE;
+  getaddrinfo(NULL, PORT, hints, &results);
+
+  //create the socket
+  int clientd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	
+  //this code should get around the address in use error
+  int yes = 1;
+  int sockOpt =  setsockopt(clientd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  err(sockOpt,"sockopt error");
+
+  //bind the socket to address and port
+  bind(clientd, results->ai_addr, results->ai_addrlen);
+  
+  //set socket to listen state
+  listen(clientd, 100);
+
+  //free the structs used by getaddrinfo
+  free(hints);
+  freeaddrinfo(results);
+  
+  return clientd;
+}
--- server.c
+++ server.c
@@ -1,1 +1,14 @@
 #include "networking.h"
+
+int main(int argc, char *argv[] ) { 
+  int listen_socket = server_setup(); 
+  while (1) {
+    int client_socket = server_tcp_handshake(listen_socket);
+    pid_t f = fork();
+    if (f == 0) {
+      printf("Forked\n");
+      subserver_logic(client_socket);
+      close(client_socket);
+    }
+  }
+}
