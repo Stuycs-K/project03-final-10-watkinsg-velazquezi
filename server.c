03bd1acf888894db49c32f3a552dc99731a33ba9
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-01-08 15:35:43 -0500

    progress on communication to multiple clients

--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include "networking.h"
-
+// TODO: start in a 'lobby' mode that doesn't send/recieve data to clients - only connects
+// when reading from stdin check if the command is 'start', if so, set a bool to true and send tasks to client (then start reading)
 int main(int argc, char *argv[] ) { 
   int listen_socket = server_setup();
   socklen_t sock_size;
@@ -9,18 +10,33 @@ int main(int argc, char *argv[] ) {
   FD_ZERO(&read_fds);
   FD_SET(STDIN_FILENO, &read_fds);
   FD_SET(listen_socket, &read_fds);
+  int cli_socks[10] = {0}; // make the max number of clients a macro?
   while (1) {
     select(listen_socket+1, &read_fds, NULL, NULL, NULL);
     if (FD_ISSET(listen_socket, &read_fds)) {
       int client_socket = server_tcp_handshake(listen_socket);
       printf("client connected.\n");
       FD_SET(client_socket, &read_fds);
+      // add client socket to array (first open spot? find it?)
     }
     if (FD_ISSET(STDIN_FILENO, &read_fds)) {
       char input[100];
       fgets(input, sizeof(input), stdin); // use read()?
       printf("input: %s\n", input);
     }
+    // for every client descriptor stored, check if it has data to read
+    for (int i = 0; i < 10; i++) {
+      if (FD_ISSET(cli_socks[i], &read_fds)) {
+        // check if client disconnected (read() returns 0), if so then remove from array with remove()
+        if (/* INSERT CONDITION FOR DISCONNECT */) {
+          removeIndex(cli_socks, 10, i);
+        } else {
+          subserver_logic(cli_socks[i]);
+        }
+      }
+    }
+
+    /
 
       // ???
       // figure out how to continually listen to clients while also having an initial 'lobby' before sending task packets out
