d0ed8ba7e065e7369db025374aadd6455e4aef49
Israel Velazquez <Ivel2018@gmail.com>
2024-01-10 15:30:44 -0500

    Returned biggest instead of Index

--- util.c
+++ util.c
@@ -23,14 +23,12 @@ void appendArr(int arr[], int newData) {
 
 int findHighest(int arr[], int size) {
   int biggest = -1;
-  int index = -1;
   for (int i=0; i<size; i++) {
     if (arr[i]>biggest) {
       biggest = arr[i];
-      index = i;
     }
   }
-  return index;
+  return biggest;
 }
 
 void bogoSort(int arr[], int size, int random, int copy[]) {
